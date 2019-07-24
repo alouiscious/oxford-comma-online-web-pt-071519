fd60b93776cb3a1d41a7b54c897dcb6426b6bdd7
Renee Sims <info@reneesoul.com>
2019-07-24 07:19:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+
+def oxford_comma(array_to_string)
+
+  last_index = array_to_string.last
+
+if array_to_string.size == 1
+  array_to_string[0]
+
+  elsif array_to_string.size == 2
+    array_to_string.join(" and ")
+
+  else
+    array_to_string[-1] = "and #{array_to_string.last}"
+    array_to_string.join(", ")
+  end
 
 end
